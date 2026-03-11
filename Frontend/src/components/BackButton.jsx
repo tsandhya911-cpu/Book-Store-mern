d8fa109f36ce227f60033b4d03c553a1ab4e7ca0
sandhya <tsandhya911@gmail.com>
2026-03-11 14:06:46 +0530

    removed commented code from frontend

--- Frontend/src/components/BackButton.jsx
+++ Frontend/src/components/BackButton.jsx
@@ -1,21 +1,4 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { IoArrowBack } from "react-icons/io5";
 
-// const BackButton = ({ destination = "/" }) => {
-//     return (
-//         <div className="flex">
-//             <Link
-//                 to={destination}
-//                 className="bg-sky-800 text-white px-4 py-1 rounded-lg w-fit"
-//             >
-//                 <IoArrowBack className="text-2xl" />
-//             </Link>
-//         </div>
-//     );
-// };
-
-// export default BackButton;
 
 import React from "react";
 import { Link } from "react-router-dom";
